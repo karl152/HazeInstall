f1ff0d27d27d78ca60225e1474d597bec129bc1b
Karl <karl.david.wes@icloud.com>
2026-01-17 12:06:10 +0100

    make it actually working

--- hazeinstall.sh
+++ hazeinstall.sh
@@ -5,6 +5,7 @@
 # Licensed under the X11 License
 # SPDX-License-Identifier: X11
 
+echo ""
 echo "HazeInstall by karl152"
 echo "**********************"
 echo "This script installs the HaZe window manager. Please choose a color for the installation script."
@@ -31,23 +32,35 @@ select col in "${colors[@]}"; do
             ;;
     esac
 done
+echo -e "$color -> updating system..."
+sudo apt-get update
+sudo apt-get dist-upgrade -y
+echo -e "$color -> installing wget..."
+sudo apt-get install wget -y
 echo -e "$color -> downloading HaZe..."
 wget http://www.escomposlinux.org/jes/haze_02/haze_0.2.tar.gz
 echo -e "$color -> checking if HaZe was downloaded correctly..."
 sha256sum --check haze_0.2.tar.gz.sha256
 echo -e "$color -> extracting archive..."
 tar -xzvf haze_0.2.tar.gz
-echo -e "$color -> updating system..."
-sudo apt-get update
-sudo apt-get dist-upgrade -y
 echo -e "$color -> installing X.Org..."
 sudo apt-get install xorg -y
 echo -e "$color -> installing X11 tools..."
-sudo apt-get install xinit x11-server-utils xterm x11-apps x11-utils -y
+sudo apt-get install xinit x11-xserver-utils xterm x11-apps x11-utils -y
 echo -e "$color -> installing HaZe dependencies and build tools..."
 sudo apt-get install build-essential libx11-dev libxft-dev libxinerama-dev libxpm-dev libxext-dev pkg-config x11proto-dev checkinstall -y
-echo -e "$color -> compiling HaZe..."
+echo -e "$color -> patching Makefile and source code..."
 cd haze_0.2
 sed -i 's|/usr/X11R6/include|/usr/include|g' Makefile
+sed -i '155,$d' Makefile
+sed -i '1i #include <string.h>' src/balloon.c
+sed -i '1i #include <string.h>' src/wild.c
+echo -e "$color -> compiling HaZe..."
 make
-sudo checkinstall #TODO
+echo -e "$color -> creating configuration..."
+echo "~/haze_0.2/hazewm" > .xinitrc
+touch ~/.hazerc ~/.Xauthority
+chmod +x starthaze
+./starthaze
+startx
+# sudo checkinstall #TODO
