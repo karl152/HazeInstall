753504af44049e10a949c471a3f8de02f572216e
Karl <karl.david.wes@icloud.com>
2026-03-04 22:15:15 +0100

    use variable for package manager
    This allows the easy use of apt, aptitude or nala instead of apt-get, which will still be the default preference.

--- hazeinstall.sh
+++ hazeinstall.sh
@@ -5,6 +5,8 @@
 # Licensed under the X11 License
 # SPDX-License-Identifier: X11
 
+pkgman="apt-get"
+
 echo ""
 echo "HazeInstall by karl152"
 echo "**********************"
@@ -33,10 +35,10 @@ select col in "${colors[@]}"; do
     esac
 done
 echo -e "$color -> updating system..."
-sudo apt-get update
-sudo apt-get dist-upgrade -y
+sudo $pkgman update
+sudo $pkgman dist-upgrade -y
 echo -e "$color -> installing wget..."
-sudo apt-get install wget -y
+sudo $pkgman install wget -y
 echo -e "$color -> downloading HaZe..."
 wget http://www.escomposlinux.org/jes/haze_02/haze_0.2.tar.gz
 echo -e "$color -> checking if HaZe was downloaded correctly..."
@@ -44,11 +46,11 @@ sha256sum --check haze_0.2.tar.gz.sha256
 echo -e "$color -> extracting archive..."
 tar -xzvf haze_0.2.tar.gz
 echo -e "$color -> installing X.Org..."
-sudo apt-get install xorg -y
+sudo $pkgman install xorg -y
 echo -e "$color -> installing X11 tools..."
-sudo apt-get install xinit x11-xserver-utils xterm x11-apps x11-utils -y
+sudo $pkgman install xinit x11-xserver-utils xterm x11-apps x11-utils -y
 echo -e "$color -> installing HaZe dependencies and build tools..."
-sudo apt-get install build-essential libx11-dev libxft-dev libxinerama-dev libxpm-dev libxext-dev pkg-config x11proto-dev checkinstall -y
+sudo $pkgman install build-essential libx11-dev libxft-dev libxinerama-dev libxpm-dev libxext-dev pkg-config x11proto-dev checkinstall -y
 echo -e "$color -> patching Makefile and source code..."
 cd haze_0.2
 sed -i 's|/usr/X11R6/include|/usr/include|g' Makefile
